1b1d6dc0a6b082be5b66b4d1b76021e7336b07e1
juhye <wonjh0730@naver.com>
2020-12-17 17:25:56 +0900

    숫자야구 -바닐라 자바스크립트와 리액트

---
+++ NumberBall.jsx
@@ -0,0 +1,74 @@
+const React = require('react');
+
+//랜덤 숫자값 내기 ( 1~9의 값), 중복 없이
+function getRandomInt(min, max) {
+    min = Math.ceil(min); // 정수 보장
+    max = Math.floor(max);// 정수보장
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+//num 만큼의 숫자 랜덤 생성
+function saveRandomInt(num){
+    let array = new Array(num).fill(0);
+    for(let i=0; i < num; i++){ 
+
+        let newInt = getRandomInt(1,9);
+        
+        while (array.includes(newInt) === true) {
+            newInt = getRandomInt(1,9);
+        }
+        
+        array[i] = newInt;
+    }
+    console.log("array", array);
+    return array;
+}
+class NumberBall extends React.Component{
+    state = {
+        value: '',
+        answer: saveRandomInt(4),
+        result: '',
+    }
+
+    //퍼블릭 클래스 필드 문법  @babel/plugin-proposal-class-properties
+    onChange = (e) => {
+        this.setState({ value: e.target.value });
+    }
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        let userInput = this.state.value;
+
+        let result = new Array(2).fill(0);
+        let answer = this.state.answer;
+        console.log(answer);
+
+        //check answer
+        console.log("value", userInput);
+        answer.forEach((answerValue, answerIndex) => {
+            Array.from(userInput).forEach((inputValue, inputIndex) => {
+                console.log(answerValue, inputValue);
+                if( answerValue === inputValue*1){
+                    if( answerIndex === inputIndex) result[0] += 1;
+                    else{ result[1] +=1; }
+                }
+            })
+        })
+
+        this.setState({
+            result: result
+        })
+    }
+    render(){
+        return (
+            <>
+                <form onSubmit={(e)=> this.onSubmitForm(e)}>
+                    <input
+                        onChange={(e) => this.onChange(e)}/>
+                    <button>입력!</button>
+                </form>
+                <span>{this.state.result[0]} 스트라이크 {this.state.result[1]} 볼 </span>
+            </>
+        )
+    }
+}
+
+module.exports = NumberBall;
